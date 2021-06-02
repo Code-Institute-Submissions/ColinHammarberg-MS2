3c28ed01544c70d189260d4234370fce7c86eb2d
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-06-02 13:32:41 +0000

    Added some more JavaScript to my email.js file. But having trouble with getting it to work. I'll dive more into that later on

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("colin", "rasmus", {
+         "from_name": contactForm.contact-name.value,
+         "from_email": contactForm.contact-email.value,
+         "project_request": contactForm.contact-project.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+}
