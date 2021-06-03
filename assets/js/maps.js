d042738c64085a9d1009bcfe1196f579c8ff5fc8
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-06-03 13:15:50 +0000

    Working on the Google Map JavaScript API and getting the locations set. Currently not working, but will get to that later today. I also realized that my #manu was not set to position: fixed, so I'm going to change that later on as well. Pushing this to git, so that I'm not losing any code

---
+++ assets/js/maps.js
@@ -0,0 +1,18 @@
+
+    function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: -34.397, lng: 150.644 },
+        { lat: -35.397, lng: 149.844 },
+        { lat: -36.397, lng: 148.944 }
+    ];
+
+    
+
