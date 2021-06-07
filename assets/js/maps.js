c66e8342a170828085ba54df31b7bac041e35320
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-06-07 09:23:49 +0000

    Currently working on fixing a marker for my Google map javaScript API. Waiting for the code institute tutor to give me some input

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,8 +10,13 @@
 
     var locations = [
         { lat: 40.7400267, lng: -73.9935017 },
-        { lat: -35.397, lng: 149.844 },
-        { lat: -36.397, lng: 148.944 }
     ];
 
-    
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
